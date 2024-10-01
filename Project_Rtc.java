764d70d89a76a12c21a089a639e0b13ab5e64145
basavarajgaddi <gaddibasavaraj101@gmail.com>
2024-10-01 11:09:42 +0100

    pushing code

---
+++ Project_Rtc.java
@@ -0,0 +1,11 @@
+public class Project_Rtc{
+
+          public static void main(String args[]){
+
+           
+          System.out.println(" Project_Rtc_Module_Using_Arduino");
+
+          }
+
+
+}
---
+++ TotalPrice.java
@@ -0,0 +1,12 @@
+public class TotalPrice{
+
+
+  public static void main(String args[]){
+  String productName="Mobile";
+  int price=10000;
+  int numberOfProducts=10;
+  int totalPrice=price * numberOfProducts;
+  System.out.println("product name:"+productName);
+  System.out.println("total price is:"+totalPrice); 
+}
+}
